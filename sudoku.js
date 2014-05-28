68f87b76ee402e27ff1bd3bc715513b59be99393
Tian Song <tian.y.song@gmail.com>
2014-05-28 11:04:28 -0700

    we can initialize a Square

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,29 @@
 module.exports = {};
 
-var Sudoku = function () {
-
+var Sudoku = function() {
 };
 
-var squares = [];
-var square = {
+var Square = function(num) {
+	this.row = '';
+	this.column = '';
+	this.region = '';
+	this['1'] = '';
+	this['2'] = '';
+	this['3'] = '';
+	this['4'] = '';
+	this['5'] = '';
+	this['6'] = '';
+	this['7'] = '';
+	this['8'] = '';
+	this['9'] = '';
 
+	if (num !== " ") {
+		this[num] = true;
+	} 
 };
+
+var allSquares = [];
+
 
 
 var str = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
@@ -19,5 +35,8 @@ module.exports.createStringArray = function(str) {
 	return array;
 };
 
+testSqaure = new Square('1');
+
+console.log(testSqaure);
 // var game = new Sudoku(str);
 // game.solve();
