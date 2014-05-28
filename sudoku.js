0fbee89242172a5f7ac73bdf63a2a9ce79b2cb55
Tian Song <tian.y.song@gmail.com>
2014-05-28 13:31:10 -0700

    populates allSquares array

--- sudoku.js
+++ sudoku.js
@@ -8,21 +8,22 @@ var Square = function(num, index) {
 	this.row = Math.floor(index / 9) + 1;
 	this.column = (index % 9) + 1;
 	this.region = determineRegion(this.row, this.column);
-	if (str !== " ") {
+	if (num !== " ") {
 		var i = 1;
 		while (i < 10) {
 			this[i.toString()] = false;
 			i++;
 		}
 		this[num] = true;
+		this.solution = parseInt(num);
 	} 
 };
 
 var allSquares = [];
 
-var str = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
+var inputString = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
 
-module.exports.createStringArray = function(str) {
+var createStringArray = function(str) {
 	var array = [];
 	array = str.split('');
 	return array;
@@ -49,7 +50,13 @@ var determineRegion = function(row, column) {
 	return intersect[0];
 };
 
-testSqaure = new Square('1', 30);
+var stringArray = createStringArray(inputString);
+for(var i = 0; i < 81; i++) {
+	var square = new Square(stringArray[i], i);
+	allSquares.push(square);
+}
+
+testSqaure = new Square(' ', 30);
 
 console.log(testSqaure);
 // var game = new Sudoku(str);
